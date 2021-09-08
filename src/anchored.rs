60eee6d34048cae2c5d4da64cf024e33d1c5cfdf
Ruihang Xia <waynestxia@gmail.com>
2021-09-08 21:00:34 +0800

    trait & struct

---
+++ src/anchored.rs
@@ -0,0 +1,29 @@
+use std::ops::{Deref, DerefMut};
+
+pub auto trait Unanchored {}
+
+pub struct Anchored<T> {
+    data: T,
+}
+
+impl<T> !Unanchored for Anchored<T> {}
+
+impl<T> Anchored<T> {
+    pub fn new(data: T) -> Self {
+        Self { data }
+    }
+}
+
+impl<T> Deref for Anchored<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<T> DerefMut for Anchored<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.data
+    }
+}
